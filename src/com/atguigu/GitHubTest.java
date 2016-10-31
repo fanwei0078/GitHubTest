6a2b58ca96d2955fd038c1c173b57fb70c5ee362
fanwei0078 <809482076@qq.com>
2016-10-31 19:51:53 +0800

    Update GitHubTest.java

--- src/com/atguigu/GitHubTest.java
+++ src/com/atguigu/GitHubTest.java
@@ -5,6 +5,8 @@ public class GitHubTest {
 	public static void main(String[] args) {
 
 			System.out.println("hello world");
+			System.out.println("hello xiaomi");
+			System.out.println("hello 123456");
 	}
 
 }
